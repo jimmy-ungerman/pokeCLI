469155ca786580e3a0d10b67908ef0bac0c87ffc
Jimmy Ungerman <jimmy.ungerman@gmail.com>
2022-12-05 20:32:29 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# pokeCLI
+The CLI tool for interacting with the Pokemon API
+
+## Motivation
+I've been working a lot lately with various CLI tools that are using Golang under the hood and figured it be good practice to try and develop one of my own using
+Cobra.
+
+This CLI tool is allowing me to learn more about interacting with APIs using go, marshaling and unmarshaling JSON in go, and the various modules written to help create your own CLI tool.
+
+## Usage
+Right now the barebones usage is running the `pokeCLI` command to get a list of all 905 different Pokemon species.
+
+## Roadmap
+* Search for Pokemon details by name
+* Search for Pokemon details by Pokedex Entry number
+* List all Pokemon by Region
+* List all Pokemon by Type
+* List all Legendary Pokemon
+* List all Starter Pokemon
---
+++ cmd/root.go
@@ -0,0 +1,80 @@
+/*
+Copyright © 2022 NAME HERE jimmy.ungerman@gmail.com
+
+*/
+package cmd
+
+import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+	"github.com/spf13/cobra"
+)
+
+// A Response struct to map the Entire Response
+type Response struct {
+    Pokemon []Pokemon `json:"results"`
+	
+}
+
+// A Pokemon Struct to map every pokemon to.
+type Pokemon struct {
+	Name	string	`json:"name"`
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "pokeCLI",
+	Short: "CLI for Pokemon API",
+	Long: `CLI to interact with the Pokemon API and get details about any pokemon.
+	
+	`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) { 
+		const baseURL = "https://pokeapi.co/api/v2/pokemon-species?limit=100000&offset=0"
+		response, err := http.Get(baseURL)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+	
+		responseData, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		var responseObject Response
+		json.Unmarshal(responseData, &responseObject)
+
+		for i := 0; i < len(responseObject.Pokemon); i++ {
+			fmt.Println(responseObject.Pokemon[i].Name)
+		}
+	
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pokeCLI.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
